42684e8897ded5f6ad266d07db278304b47278f5
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-06-18 23:48:17 +0800

    Finish logic functionality

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
+  winningNum = getRandomNum(min, max),
   guessesLeft = 3;
-
+// console.log(winningNum);
 //UI el
 const game = document.querySelector("#game"),
   minNum = document.querySelector(".min-num"),
@@ -16,15 +16,20 @@ const game = document.querySelector("#game"),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener("mousedown", function (e) {
+  if (e.target.className === "play-again") {
+    window.location.reload();
+  }
+});
+
 // listen for guess
 guessBtn.addEventListener("click", function () {
   let guess = parseInt(guessInput.value);
 
   if (isNaN(guess) || guess < min || guess > max) {
     setMessage(`Please enter a number between ${min} and ${max}.`, "red");
-  }
-  // CHeck if won
-  if (guess === winningNum) {
+  } else if (guess === winningNum) {
     // Game Over - won
     gameOver(true, `${winningNum} is correct, YOU WIN!`);
   } else {
@@ -51,12 +56,6 @@ guessBtn.addEventListener("click", function () {
     }
   }
 });
-// Set Message
-function setMessage(msg, color) {
-  message.style.color = color;
-  message.textContent = msg;
-}
-
 function gameOver(won, msg) {
   let color;
   won === true ? (color = "green") : (color = "red");
@@ -68,4 +67,17 @@ function gameOver(won, msg) {
   message.style.color = color;
   //set message
   setMessage(msg);
+
+  //Play Again
+  guessBtn.value = "Play Again";
+  guessBtn.className += "play-again";
+}
+// Get Winning Number
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+// Set Message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
 }
